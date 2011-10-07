1162c66849f53e47d80d45909151521432bd0eee
Adam Lilienfeldt <lilienfeldt.adam@gmail.com>
2011-10-07 22:02:34 +0200

    File for calling WWW files, server query

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
 
 $page = $_SERVER["QUERY_STRING"];
-if (!$page) $page = "main";
+	if (!$page) $page = "main";
 
 require("www/header.php");
-require("www/text/$page.html");
+require("www/text/"$page".html");
 require("www/footer.php");
+
 ?>

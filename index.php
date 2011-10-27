02652abbda89bdaffd1259e18e8d2d2d84ae0f34
Adam Lilienfeldt <lilienfeldt.adam@gmail.com>
2011-10-27 19:20:28 +0200

    Main files

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
 <?php
 
+//Request query string, save in $page
 $page = $_SERVER["QUERY_STRING"];
+	#If no query string, $page=main
 	if (!$page) $page = "main";
 
+
+//Require header of webpage
 require("www/header.php");
-require("www/text/"$page".html");
+//Require $page (Query string) including text for page
+require("www/text/$page.html");
+//Require main.php
+require("main.php");
+//Require footer of webpage
 require("www/footer.php");
+//
 
 ?>
